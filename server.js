fc222baa44a8f230dd48fbb4c66db4bf7f94618e
Shivang Sharma <85074821+shivang821@users.noreply.github.com>
2023-07-20 16:27:24 +0530

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv')
+const cors=require('cors')
 const express = require('express')
 const app = express()
 app.use(express.json());
@@ -11,6 +12,7 @@ require('./database/conn')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
     // app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(cors())
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(fileUpload())
 app.use(cookieParser())
