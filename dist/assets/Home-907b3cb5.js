625fb8c82e21206c29d6e49bb3595f9323d2ccef
Shivang Sharma <85074821+shivang821@users.noreply.github.com>
2023-07-25 11:11:07 +0530

    change dist

--- dist/assets/Home-907b3cb5.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-fed51376.js";const o=()=>s.jsxs("div",{className:"home",children:[s.jsx("h2",{children:"Home"}),s.jsx("div",{}),s.jsx("div",{}),s.jsx("div",{})]});export{o as default};
